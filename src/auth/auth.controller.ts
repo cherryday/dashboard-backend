4e32b9342ea68a9be2d154fdd06882b749aa082a
cherryday <tcarevmaxim@gmail.com>
2022-08-06 00:11:28 +0300

    add signin method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { UserEntity } from 'src/users/user.entity';
 import { AuthService } from './auth.service';
+import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('signin')
+  signin(@Body() signinDto: SigninDto): Promise<any> {
+    return this.authService.signin(signinDto);
+  }
+
   @Post('signup')
-  signup(@Body() signupDto: SignupDto) {
-    this.authService.signup(signupDto);
+  signup(@Body() signupDto: SignupDto): Promise<UserEntity> {
+    return this.authService.signup(signupDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,34 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { UserEntity } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
+import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async signup(signupDto: SignupDto) {
+  async signin(signinDto: SigninDto) {
+    const user = await this.usersService.findByEmail(signinDto.email);
+
+    if (!user) {
+      throw new BadRequestException('wrong email or password');
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      signinDto.password,
+      user.password,
+    );
+
+    if (!isValidPassword) {
+      throw new BadRequestException('wrong email or password');
+    }
+
+    return 'TOKEN';
+  }
+
+  async signup(signupDto: SignupDto): Promise<UserEntity> {
     const user = this.usersService.findByEmail(signupDto.email);
 
     if (user) {
@@ -18,6 +39,6 @@ export class AuthService {
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(signupDto.password, salt);
 
-    this.usersService.create({ ...signupDto, password: hashedPassword });
+    return this.usersService.create({ ...signupDto, password: hashedPassword });
   }
 }
